d8010dd0f6340f694a8cf4e3e5e52322818654ba
Akanksha2596 <akanksha.yadav@brainvire.com>
2024-06-26 16:31:47 +0530

    styles added for list container

--- src/components/Elements/ListElement/ListElement.tsx
+++ src/components/Elements/ListElement/ListElement.tsx
@@ -15,9 +15,37 @@ const ListElement = ({ listData, key }) => {
     textTransform: listStyle?.textTransform,
   };
 
+  const listItemContainer = {
+    height: style?.height,
+    width: style?.width,
+    minWidth: style?.minWidth,
+    minHeight: style?.minHeight,
+    maxWidth: style?.maxWidth,
+    maxHeight: style?.maxHeight,
+    borderStyle: style?.borderStyle,
+    borderColor: style?.borderColor,
+    borderTopWidth: style?.borderTopWidth,
+    borderTopStyle: style?.borderTopStyle,
+    borderTopColor: style?.borderTopColor,
+    borderRightWidth: style?.borderRightWidth,
+    borderRightStyle: style?.borderRightStyle,
+    borderRightColor: style?.borderRightColor,
+    borderBottomWidth: style?.borderBottomWidth,
+    borderBottomStyle: style?.borderBottomStyle,
+    borderBottomColor: style?.borderBottomColor,
+    borderLeftWidth: style?.borderLeftWidth,
+    borderLeftStyle: style?.borderLeftStyle,
+    borderLeftColor: style?.borderLeftColor,
+    borderRadius: style?.borderRadius,
+    borderTopLeftRadius: style?.borderTopLeftRadius,
+    borderTopRightRadius: style?.borderTopRightRadius,
+    borderBottomLeftRadius: style?.borderBottomLeftRadius,
+    borderBottomRightRadius: style?.borderBottomRightRadius,
+  };
+
   return (
-    <div className="le_container" key={key}>
-      <p style={style}>{label}</p>
+    <div className="le_container" key={key} style={listItemContainer}>
+      <p style={listItemTextStyle}>{label}</p>
       <ul className="list-conatiner" style={listStyle}>
         {listItemDetails?.length > 0
           ? listItemDetails?.map((item, i) => {
